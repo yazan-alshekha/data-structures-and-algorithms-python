41bd1fa771667f4bd060e7c488a89e64afa585dc
yazan <yazan.alshekha@outlook.com>
2020-08-18 00:15:30 +0300

    add read me for challenge2

--- tests/challenges/test_array_shift.py
+++ tests/challenges/test_array_shift.py
@@ -1,10 +1,9 @@
 # from data_structures_and_algorithms.challenges.array-shift.array-shift import array_shift
-from data_structures_and_algorithms.challenges.array-shift.array-shift import (
-    array-shift,
-)
+from data_structures_and_algorithms.challenges.array_shift.array_shift_basics import array_shift
+
 
 def test_array_shift():
-    expected=[0,0,3,7,8]
-    arr=[0,2,3,7,8]
-    actual=array_shift(arr,0)
-    assert expected=actual
+    expected=[0,1,2,3,7,8]
+    arr=[0,1,3,7,8]
+    actual=array_shift(arr,2)
+    assert expected==actual
